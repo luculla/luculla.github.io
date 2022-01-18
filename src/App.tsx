4631effea078d56af71697a50f2427026c1745d5
Lucille Guthrie <59029306+luculla@users.noreply.github.com>
2022-01-18 20:09:08 +0000

    QoL, Added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,27 @@ function App() {
     setPatt(event.target.value);
   }
   const rows=useMemo(()=>{
-      return patt.split("|")
+      return patt.split("$")
   },[patt])
   return (
     <div className="App p-4">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <div className="mb-5">Welcome to PlaceMarker</div>
+        <div className="my-4">Welcome to PlaceMarker</div>
       </header>
       <div className="row"><div className="col">
-        Enter your pattern below, delimiting each line with a "|" (pipe)
+        <br/>
+        Enter your pattern below, delimiting each line with a "$"
+        <br/>
+        <br/>
+        If you are using a word processor to edit your pattern into this format; you can "Find and replace" (ctrl + h) the line breaks by entering "^p" into the find box and "$" into the replace box.
+        <br/>
         <br/>
         <textarea className="form-control" style={{height: "400px"}} value={patt} onChange={handlepatt}/>
         <br/>
         <RowStepper rows={rows}/>
+        <br/>
+        <a href='https://ko-fi.com/waywardfacets' target='_blank'><img height='35' style={{border:"0px",height:"46px"}} src='https://az743702.vo.msecnd.net/cdn/kofi3.png?v=0' alt='Buy Me a Coffee at ko-fi.com'/></a>
       </div></div>
     </div>
   );
--- src/Components/RowStepper.tsx
+++ src/Components/RowStepper.tsx
@@ -12,13 +12,23 @@ export default function RowStepper({rows}:RowStepperProps){
         const canNext=useMemo(()=>{
             return rows.length && rowIndex < (rows.length -1)
         },[rowIndex,rows])
+        function backRow(){
+            setRowIndex(rowIndex-1)
+        }
+        const canPrev=useMemo(()=>{
+            return rowIndex > 0
+        },[rowIndex,rows])
         function homeRow(){
             setRowIndex(0)
         }
         return<>
         <div>{rows[rowIndex]}</div>
         <br/>
-        <button className="btn btn-secondary" onClick={nextRow} disabled={!canNext}>Next Row</button>
+        <div className="row align-items-center">
+            <div className="col d-grid"><button className="btn btn-secondary" onClick={backRow} disabled={!canPrev}>Previous<br/>Row</button></div>
+            <div className="col">{rowIndex+1} / {rows.length}</div>
+            <div className="col d-grid"><button className="btn btn-secondary" onClick={nextRow} disabled={!canNext}>&nbsp;&nbsp;Next&nbsp;&nbsp;<br/>Row</button></div>
+        </div>
         <br/>
         <button className="btn btn-secondary" onClick={homeRow}>Restart</button>
         </>
