e1a5d065c0c0a332ebb44654edec69828f7f4535
Lucille Guthrie <59029306+luculla@users.noreply.github.com>
2022-01-18 20:54:59 +0000

    Versioning

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
     <div className="App p-4">
       <header className="App-header">
         <div className="my-4">Welcome to PlaceMarker</div>
+        <div className="row form-text justify-content-end w-100"><div className="col-auto">Last Updated: 18-Jan-2022</div></div>
       </header>
       <div className="row"><div className="col">
         <br/>
@@ -32,7 +33,7 @@ function App() {
         <br/>
         <RowStepper rows={rows}/>
         <br/>
-        The pattern is stored locally on your device. If you clear your local storage, all data will vanish (and you may lose your place on your pattern)
+        The pattern is stored locally on your device. If you clear your local storage, all data will vanish (and you may lose your place on your pattern)<br/>
         <a href='https://ko-fi.com/waywardfacets' target='_blank'><img height='35' style={{border:"0px",height:"46px"}} src='https://az743702.vo.msecnd.net/cdn/kofi3.png?v=0' alt='Buy Me a Coffee at ko-fi.com'/></a>
       </div></div>
     </div>
